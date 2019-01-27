ff32de6ad4c03169f374e21c9bd8bb4ce75f25b6
MontyPython28 <samyusubs@gmail.com>
2019-01-27 15:31:32 +0800

    added

---
+++ blog.py
@@ -0,0 +1,3 @@
+from wordc import wordc
+
+
---
+++ wordc/wordc.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_wtf import FlaskForm, Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import os
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+class GetLetters(FlaskForm):
+    letters = StringField('Enter letters', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+def get_words(letter_list, dictionary_name):
+        lst = list()
+        sort_lst = list()
+        
+        fhand = open(dictionary_name)
+
+        for line in fhand:
+            line = line.rstrip()
+            lst.append(line)
+        
+        for item in lst:
+            test = list(letter_list)
+            count = 0
+            for character in item:
+                if character in test:
+                    count = count + 1
+                    test.remove(character)
+            if count == len(item) and len(item)>=3:
+                sort_lst.append(item)
+                print (item)
+        return sort_lst	
+	
+@app.route('/')	
+@app.route('/wordcookies', methods=['GET', 'POST'])
+def wordcookies():
+    form = GetLetters()
+    if form.validate_on_submit():
+        lst = list()
+        n = 0
+        for letter in form.letters.data:
+            lst.append(letter.upper()) #makes list of characters in upper case
+        word_list = get_words(lst, 'word_bank.txt') #gets list of words possible to make with these letters
+        return render_template('wordcookies-output.html', title='Results', word_list=word_list)
+    return render_template('wordcookies-input.html', title='Word Cookies', form=form)
+
+
+if __name__ == "__main__":
+    app.run()
